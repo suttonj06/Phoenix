5adb606de896835f711a271047bf6fbf1ca15a7f
James Sutton <suttonj06@gmail.com>
2020-07-10 21:24:24 -0400

    Give game information on all clients

--- start.js
+++ start.js
@@ -10,15 +10,17 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(process.env.PORT || 5000);
 console.log("Server started");
  
- 
 var io = require('socket.io')(serv,{});
+
 io.sockets.on('connection', (socket) => {
     console.log("Player Connected.  Issuing ID");
     var givenID = Math.ceil(Math.random() * 10);
     socket.emit('playerID', { ID: givenID });
     console.log('Player ' + givenID + ' has joined.');
+    io.sockets.emit('gameInfo', 'Player ' + givenID + ' has joined.');
  
     socket.on('attack', (data) => {
         console.log('Player ' + data.ID + ' is attacking');
+        io.sockets.emit('gameInfo', 'Player ' + data.ID + ' is attacking');
     });   
 });
