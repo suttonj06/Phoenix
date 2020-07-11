6bf2df1fd3a23d8924a4af9e0e76f91c1f499d7f
James Sutton <suttonj06@gmail.com>
2020-07-10 20:37:39 -0400

    Basic Socket.io Connection from Server/Client

---
+++ start.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+ 
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+ 
+serv.listen(2000);
+console.log("Server started");
+ 
+ 
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', (socket) => {
+    console.log("Player Connected.  Issuing ID");
+    var givenID = Math.ceil(Math.random() * 10);
+    socket.emit('playerID', { ID: givenID });
+    console.log('Player ' + givenID + ' has joined.');
+ 
+    socket.on('attack', (data) => {
+        console.log('Player ' + data.ID + ' is attacking');
+    });   
+});
